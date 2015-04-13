5ba3fcf1bc2b20d1c81f944b7236af76296941c0
kanglieyong <kanglieyong@gmail.com>
2015-04-13 23:56:23 +0800

    mysql test file

--- Database/connect1.c
+++ Database/connect1.c
@@ -3,4 +3,27 @@
 
 #include <mysql/mysql.h>
 
+int main(int argc, char* argv[])
+{
+  MYSQL* conn_ptr;
+
+  conn_ptr = mysql_init(NULL);
+  if (!conn_ptr) {
+    fprintf(stderr, "mysql_init failed\n");
+    return EXIT_FAILURE;
+  }
+
+  conn_ptr = mysql_real_connect(conn_ptr, "localhost", "jiangkui", "baishi", "songci", 0, NULL, 0);
+
+  if (conn_ptr) {
+    printf("Connection success\n");
+  } else {
+    printf("Connection failed\n");
+  }
+
+  mysql_close(conn_ptr);
+
+  return EXIT_SUCCESS;
+}
+
 
